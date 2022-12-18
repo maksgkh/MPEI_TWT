285684bd7be21572b9887da4746ac588a2bcc49d
maksgkh <maksgkh@gmail.com>
2022-12-18 13:25:54 +0300

    main

---
+++ website/mainapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request,'index.html')
+
+
+def history(request):
+    return render()
+
+
+def enrollee(request):
+    return render()
+
+
+def contact(request):
+    return render()
+
+
+def employment(request):
+    return render()
+
+
+def articles(request):
+    return render()
--- website/website/urls.py
+++ website/website/urls.py
@@ -1,21 +1,13 @@
-"""website URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from mainapp import views
 
 urlpatterns = [
+    path('', views.index,name='home'),
+    path('history', views.history,name='history'),
+    path('enrollee', views.enrollee,name='enrollee'),
+    path('contact', views.contact,name='contact'),
+    path('employment', views.employment,name='employment'),
+    path('articles', views.articles,name='articles'),
     path('admin/', admin.site.urls),
 ]
